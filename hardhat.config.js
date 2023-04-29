2b182e2ccffcf6f49fc800a8922cce9fb8b52898
Matheus Ferraz <ferroca@pop-os.localdomain>
2023-04-29 13:37:48 -0300

    add rpc

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,8 @@ module.exports = {
       chainId: 1337,
     },
     mumbai: {
-      
+      url: "https://polygon-mumbai.infura.io/v3/2264d3781588440fa12e3aa8b3d1030a",
+      accounts: [process.env.PRIVATE_KEY],
     }
   },
 };
